d167370626d5399310f264880b8db3f1a0a597d3
HelenaPruna <72511529+HelenaPruna@users.noreply.github.com>
2024-09-30 12:56:39 +0200

    Semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,18 +1,31 @@
 
 import './App.css'
-import {ducks} from "./demo.ts";
-import DuckItem from "./duckItem.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Header, List, ListItem} from "semantic-ui-react";
+
 
 function App() {
-
-  return (
-      <div>
-        <h1>Reactivities</h1>
-        {ducks.map(duck => (
-            <DuckItem key = {duck.name} duck = {duck}/>
-        ))}
-      </div>
-  )
+    const [activities, setActivities] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities')
+            .then(response => {
+                setActivities(response.data)
+            })
+    }, []);
+    
+    return (
+        <div>
+            <Header as='h2' icon={'users'} content={'Reactivities'}/>
+            <List>
+                {activities.map((activity:any) => (
+                    <ListItem key ={activity.id} >
+                        {activity.title}
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    )
 }
 
 export default App
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'semantic-ui-css/semantic.min.css'
 import App from './App.tsx'
 import './index.css'
 
