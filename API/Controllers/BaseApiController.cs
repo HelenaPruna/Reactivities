0b9449c388b4c687ccd2a9927af55a1fc9174c94
HelenaPruna <72511529+HelenaPruna@users.noreply.github.com>
2024-10-09 13:48:31 +0200

    End Section 10

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,14 +1,40 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController: ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class BaseApiController: ControllerBase
+    private IMediator _mediator;
+    protected IMediator Mediator => _mediator ??= 
+        HttpContext.RequestServices.GetService<IMediator>();
+
+    /*protected ActionResult HandleResult<T>(Result<T> result)
     {
-        private IMediator _mediator;
-        protected IMediator Mediator => _mediator ??= 
-            HttpContext.RequestServices.GetService<IMediator>();
+        if (result.IsSuccess && result.Value != null)
+            return Ok(result.Value);
+        if (result.IsSuccess && result.Value == null)
+            return NotFound();
+        return BadRequest(result.Error);
+    }*/
+    
+    
+    //prov solution as tutorial didn't solve this at this stage for me xxx
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+        if (result == null)
+            return BadRequest("Result object is null."); 
+
+        if (result.IsSuccess && result.Value != null)
+            return Ok(result.Value);
+
+        if (result.IsSuccess && result.Value == null)
+            return NotFound();
+
+        return BadRequest(result.Error);
     }
+
 }
